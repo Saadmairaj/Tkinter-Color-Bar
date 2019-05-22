4facb3d62b5f12ce4f1dab9eb3a7850e78398964
Saad Mairaj <46227224+Saadmairaj@users.noreply.github.com>
2019-05-22 15:49:32 +0530

    Improved code
    Removed unnecessary lines of code and fixed visibility issues seeing the HEX or RGB on the maker.
    Fixed few bugs.

--- ColorBar.py
+++ ColorBar.py
@@ -22,14 +22,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 
-Version: 0.0.1
+Version: 0.0.2
 
 Repository:
 https://github.com/Saadmairaj/Tkinter-Color-Bar
 
 '''
 
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter.font import Font
 from tkinter import font
@@ -37,14 +37,14 @@ import numpy as np
 
 
 # Round Soild square for the canvas
-def create_rounded(self, x1, y1, x2, y2, r, color="black",**kw):
-    self.create_arc(x1,  y1,  x1+r,   y1+r, start= 90, extent=90, style=PIESLICE, outline="", **kw)
-    self.create_arc(x2-r-1, y1, x2-1, y1+r, start=  0, extent=90, style=PIESLICE, outline="", **kw)
-    self.create_arc(x1, y2-r-1, x1+r, y2-1, start=180, extent=90, style=PIESLICE, outline="", **kw)
-    self.create_arc(x2-r, y2-r, x2-1, y2-1, start=270, extent=90, style=PIESLICE, outline="", **kw)
+def create_rounded(self, x1, y1, x2, y2, r,**kw):
+    self.create_arc(x1,  y1,  x1+r,   y1+r, start= 90, extent=90, style='pieslice', outline="", **kw)
+    self.create_arc(x2-r-1, y1, x2-1, y1+r, start=  0, extent=90, style='pieslice', outline="", **kw)
+    self.create_arc(x1, y2-r-1, x1+r, y2-1, start=180, extent=90, style='pieslice', outline="", **kw)
+    self.create_arc(x2-r, y2-r, x2-1, y2-1, start=270, extent=90, style='pieslice', outline="", **kw)
     self.create_rectangle(x1+r/2, y1, x2-r/2, y2, width=0, **kw)
     self.create_rectangle(x1, y1+r/2, x2, y2-r/2, width=0, **kw)
-Canvas.create_rounded = create_rounded
+tk.Canvas.create_rounded = create_rounded
 
 # Round Rectanle for the canvas
 def rounded_rect(self, x, y, w, h, c, fill="black", linetags=None, arctags=None, **kw):
@@ -56,91 +56,59 @@ def rounded_rect(self, x, y, w, h, c, fill="black", linetags=None, arctags=None,
     self.create_line(x+c, y+h, x+w-c, y+h  , fill=fill, **kw)
     self.create_line(x,   y+c, x,     y+h-c, fill=fill, **kw)
     self.create_line(x+w, y+c, x+w,   y+h-c, fill=fill, **kw)
-Canvas.create_roundsqaure = rounded_rect
+tk.Canvas.create_roundsqaure = rounded_rect
 
 
-
-class Colorscale(Canvas):
+class Colorscale(tk.Canvas):
     """
-        Color Scale for tkinter
-        Use mousewheel to be presice
-        "mousewheel" = False    # Disabled
-        "mousewheel" = True     # Enabled
+    Color Scale.
+    ------
+        1.  Use mousewheel to be presice.
+            Configure: -
+                mousewheel = False    # Disabled
+                mousewheel = True     # Enabled
 
-        Returns hexcode or RGB 
-        if value = "hex" or value = "rgb" respectively
-    """
+        2.  Returns HEX code or RGB 
+            Configure: -
+                value = "hex"    # HEX
+                value = "rgb"    # RGB 
+
+        To configure use Colorscale.config or Colorscale.configure
+        but not like Colorscale['width'] = 100."""
+    
     def __init__(self, *arg, **kw):
-        
-        self.val = kw.get("value", "rgb")
-        self.command = kw.get("command")
+        self.val = kw.pop("value", "rgb")
+        self.command = kw.pop("command", None)
         self.width = kw.get("width", 200)
         self.height = kw.get("height", 24)
-        self._Mw_bool = kw.get("mousewheel", True)
-
-        kw.pop("value", None)
-        kw.pop("command", None)
-        kw.pop("mousewheel", None)
-
-        Canvas.__init__(self, *arg, **kw)
+        self._Mw_bool = kw.pop("mousewheel", True)
+        self._remove_id = None
+        tk.Canvas.__init__(self, *arg, **kw)
         self.config(highlightthickness=0)
         self.config(width=self.width, height=self.height)
-
         self._im = np.load("colorscale.npy")
-        self._im = Image.fromarray(self._im)
-        self._im = np.array(self._im)
-        self._im = Image.fromarray(self._im)
-        self._im = self._im.resize((self.width, self.height))
+        self._im = Image.fromarray(self._im).resize((self.width, self.height))
         self.pixels = self._im.load()
         self._im = ImageTk.PhotoImage(self._im)
-
         self.create_image(self.width/2,self.height/2, image=self._im)
         self.image = self._im
-
         self.create_roundsqaure(self.width/3 ,2, 5, self.height-4, 2, width=2, 
-            fill="white", tags="marker")
-
+            fill="black", tag="marker")
         self.bind("<B1-Motion>", self.move_marker)
-        self.bind("<ButtonRelease-1>", self.Release,"+")
-        if self._Mw_bool:
-            self.bind("<MouseWheel>", self.MouseWheel)
+        if self._Mw_bool: self.bind("<MouseWheel>", self.MouseWheel)
         self.xaxis = int(self.width/3)
 
-    def configure(self, cnf=None, **kw):
-        self.val = kw.get("value", self.val)
-        self.command = kw.get("command", self.command)
+    def configure(self, cnf={}, **kw):
+        kw = tk._cnfmerge((cnf, kw))
+        self.val = kw.pop("value", self.val)
+        self.command = kw.pop("command", self.command)
         self.width = kw.get("width", self.width)
         self.height = kw.get("height", self.height)
-        self._Mw_bool = kw.get("mousewheel", self._Mw_bool)
-
-        kw.pop("value", None)
-        kw.pop("command", None)
-        kw.pop("mousewheel", None)
-
-        if self._Mw_bool:
-            self.bind("<MouseWheel>", self.MouseWheel)
-        else: 
-            self.unbind("<MouseWheel>")
-
-        return super().configure(cnf=cnf, **kw)
-    
-    def config(self, cnf=None, **kw):
-        self.val = kw.get("value", self.val)
-        self.command = kw.get("command", self.command)
-        self.width = kw.get("width", self.width)
-        self.height = kw.get("height", self.height)
-        self._Mw_bool = kw.get("mousewheel", self._Mw_bool)
-
-        kw.pop("value", None)
-        kw.pop("command", None)
-        kw.pop("mousewheel", None)
-
-        if self._Mw_bool:
-            self.bind("<MouseWheel>", self.MouseWheel)
-        else: 
-            self.unbind("<MouseWheel>")
-
-        return super().config(cnf=cnf, **kw)
+        self._Mw_bool = kw.pop("mousewheel", self._Mw_bool)
+        if self._Mw_bool: self.bind("<MouseWheel>", self.MouseWheel)
+        else: self.unbind("<MouseWheel>")
+        return super().configure(**kw)
+    config = configure
 
     def Release(self, evt=None):
         "Internal function."
@@ -148,7 +116,7 @@ class Colorscale(Canvas):
         self.delete("markerbg")
 
     def RGBtoHEX(self, R,G,B):
-        "Internal function."
+        "Internal function. Used to convert RGB to HEX"
         return '#%02x%02x%02x' % (R, G, B)
 
     def move_marker(self, evt, mw=None): 
@@ -156,20 +124,15 @@ class Colorscale(Canvas):
         if mw: 
             evt.x = mw
             evt.y = 10
-            self.count = 10
         
-            def close():
-                if self.count <= 0:
-                    self.Release()
-                    return
-                self.count-=1
-                self.after(500, close)
-            close()
+        if self._remove_id: self.after_cancel(self._remove_id)
+        self._remove_id = self.after(1000, self.Release)
 
         if (evt.x>0 and evt.y>0 and evt.x<self.width and evt.y<self.height):
             if not mw: self.xaxis = evt.x
             RGB = self.pixels[evt.x,evt.y][:-1]
             hexcode = self.RGBtoHEX(RGB[0], RGB[1], RGB[2])
+            maker_color = 'black' if (RGB[0]*0.299 + RGB[1]*0.587 + RGB[2]*0.114) > 110 else 'white'
 
             if self.val == "rgb":
                 spacer = 50
@@ -183,28 +146,24 @@ class Colorscale(Canvas):
                 self.Return_value(hexcode)
 
             self.delete("marker")
-            self.delete("markerbg")
-            self.delete("info")
+            self.Release()
             self.create_roundsqaure(evt.x ,2, 5, self.height-4, 2, width=2, 
-                fill="white",tags="marker")
+                fill=maker_color,tag="marker")
 
             if evt.x < self.width-100:
                 self.create_rounded(evt.x+spacer-spacbg, self.height/2-6, evt.x+spacer+spacbg, 
-                    self.height/2+7, 6,fill="white", tags="markerbg")
+                    self.height/2+7, 6,fill=maker_color, tag="markerbg")
                 self.create_text(evt.x+spacer, self.height/2, text=text, 
-                    font=Font(size=10), tags="info", fill=hexcode)
+                    font=Font(size=10), tag="info", fill=hexcode)
             else: 
                 self.create_rounded(evt.x-spacer-spacbg, self.height/2-6, evt.x-spacer+spacbg, 
-                    self.height/2+7, 6, fill="white", tags="markerbg")
+                    self.height/2+7, 6, fill=maker_color, tag="markerbg")
                 self.create_text(evt.x-spacer, self.height/2, text=text, 
-                    font=Font(size=10), tags="info", fill=hexcode)
-
-            self.update_idletasks()
+                    font=Font(size=10), tag="info", fill=hexcode)
 
     def Return_value(self, val):
         "Internal function."
-        if self.command:
-            return self.command(val)
+        if self.command: return self.command(val)
 
     def MouseWheel(self, evt=None):
         "Internal function."
@@ -219,12 +178,12 @@ class Colorscale(Canvas):
 
 # Testing and demo
 if __name__ == "__main__":
-    root = Tk()
+    root = tk.Tk()
     root.config({"bg":"black"})
     root.geometry("320x250+100+100")
     root.title("Tkinter Color Bar")
 
-    L = Frame(root, bg="white", width=150, height=150)
+    L = tk.Frame(root, bg="white", width=150, height=150)
     L.pack(padx=10, pady=(30, 10))
 
     CS = Colorscale(
